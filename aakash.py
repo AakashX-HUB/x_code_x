3987afdf527ec539b3ff4f70de2cdc274facde29
AakashX-HUB <aakash4118poco@gmail.com>
2026-05-29 15:50:43 +0530

    Add files via upload

--- aakash.py
+++ aakash.py
@@ -1,63 +1,38 @@
-#FUNCTION
-#1
+#for creating file
+a=open(r"C:\Users\Livewire\Desktop\aakash.txt",'a')
+print("file created")
+a.write("hi aakash"
+        "1,madhan,anbu,karthi"
+        "preethi"
+        "kavi")
+a.flush()#to push data in the file
+a.close()#to close file
+print("file writer")
+#to read the file fro, the open statement
+a=open(r"C:\Users\Livewire\Desktop\aakash.txt",'r')
+print(a.read())
 
-def add():
-    a=13
-    b=12
-    print("add")
-add()
-#2
-def sub():
-    a=12
-    b=13
-    print("sub:")
-sub()    
-#3
-a=int(input("enter a"))
-b=int(input("enter b"))
-def add(a,b):
-    print(a+b)
-add(a,b)
-#4
-def details(name="jaa",course="java",clg="AIML"):
-    print(name)
-details()
-#5
-def fun(**a):
-    for i,j in a.items():
-        print(i,j)
-fun(name="abc",age=2,city="salem")
-#6
-def demo(*p):
-    for i in p:
-        print(i)
-demo()
-def fun(**a):
-    for i,j in a.items():
-        print(i,j)
-    fun(name="abc",age=2,city="salem")
-def demo(*a):
-    for i in a:
-        print(i*2)
-demo(1,2,3,4,5,6,6)        
-#7 lambda
-g=lambda a,b:a*b
-print(g(1,2))
-g1=(lambda a,b:a+b)
-print(g1(1,2))
-#8 return
-def demo():
-    for i in range(1,10):
-        return(i)
-print(demo())
-#9recurtion
-def fib(num):
-    if num==0 or num==1:
-        return num
-    elif num>=1:
-        return fib(num-2)+fib(num-1)
-print(fib(7))
-#9
+#to read line by line
+print(a.readline())
+print(a.readlines())
 
+#to delete file
+ #import os
+#os.remove(r"C:\Users\Livewire\Desktop\aakash.txt",'r')
 
-    
+#to enter the bill
+
+aakash=str(input("enter the name"))
+bill=open("C:\\Users\\Livewire\\Desktop\\\\aakash.txt",'r')
+print(bill)
+bill=open(r"C:\Users\Livewire\Desktop\aakash.txt",'a+')
+bill.write("---------virthiya shop---------")
+price=5
+count=3
+bill.write("food in data biriyani 2  $200")
+bill.write(input(str(price*count)))
+bill.flush
+bill.close()
+bill=open(r"C:\Users\Livewire\Desktop\aakash.txt",'a+')
+print(bill.read())
+a.close()
